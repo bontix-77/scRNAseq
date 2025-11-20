66cabdc7813f8e2533c39bb796e9209b5fc1954e
bontix-77 <bontix77@yahoo.it>
2025-11-20 11:43:09 -0500

    Solved a gene indexing issue in scanpy. Added gene markers per cluster image

--- Code/Seurat/HIV PBMC/HIV scanpy/seurat_to_scanpy.py
+++ Code/Seurat/HIV PBMC/HIV scanpy/seurat_to_scanpy.py
@@ -1,15 +1,17 @@
-
-'''
+"""
 tutorial:
 https://scanpy.readthedocs.io/en/latest/tutorials/index.htmlhttps://scanpy.readthedocs.io/en/latest/tutorials/index.html
-'''
+"""
 
 import scanpy as sc
 import numpy as np
-#import louvain    #library can't install: missing dependencices i can't solve
+# import louvain    #library can't install: missing dependencices i can't solve
 
 adata = sc.read_h5ad("/home/alexander-bontempo/Desktop/HIV GSM/h5ad/data.h5ad")
 
+
+# for some reason the indexes in the var is not the gene name but a numeric. Thefollowin
+adata.raw.var.set_index(adata.raw.var["_index"], inplace=True)
 ######## change gene names
 # Load gene namessc.pl.dotplot(
 #    adata,
@@ -17,14 +19,14 @@ adata = sc.read_h5ad("/home/alexander-bontempo/Desktop/HIV GSM/h5ad/data.h5ad")
 #    groupby="leiden_res_0.02",
 #    standard_scale="var",
 #    use_raw=False
-#)
+# )
 with open("/home/alexander-bontempo/Desktop/HIV GSM/h5ad/gene_names.txt") as f:
     gene_names = [line.strip() for line in f]
 
-# Replace raw.var_names
+    # Replace raw.var_names
     # adata.raw.var_names = gene_names
 
-# Optional: replace var_names too
+    # Optional: replace var_names too
     adata.var_names = gene_names
 
 
@@ -33,14 +35,13 @@ with open("/home/alexander-bontempo/Desktop/HIV GSM/h5ad/gene_names.txt") as f:
 sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
 
 # PCA
-sc.tl.pca(adata, svd_solver='arpack')
-
+sc.tl.pca(adata, svd_solver="arpack")
 
 
 # Compute neighborhood graph
 sc.pp.neighbors(adata, n_neighbors=10, n_pcs=40)
 
-#louvain clustering
+# louvain clustering
 sc.tl.louvain(adata, resolution=0.5)
 
 
@@ -49,93 +50,94 @@ sc.tl.leiden(adata, resolution=1.2)
 # 7️⃣ Compute UMAP embedding
 sc.tl.umap(adata)
 # Plot UMAP
-sc.pl.umap(adata, color='leiden')
+sc.pl.umap(adata, color="leiden")
 
 
-#how many cells per cluster?
-adata.obs['leiden'].value_counts()
+# how many cells per cluster?
+adata.obs["leiden"].value_counts()
 
 
-#cluster markers
-sc.tl.rank_genes_groups(adata, groupby='leiden', method='t-test')
-'''
-for some reason adata.uns['rank_genes_groups']['names']
+# cluster markers
+sc.tl.rank_genes_groups(adata, groupby="leiden", method="t-test")
+"""
+UPDATE: indexing solution below was partial. complete solution has been introduced by line 14
+For some reason adata.uns['rank_genes_groups']['names']
 doesn't contain gene name but the adata.raw.var['_index'].
 To convert the indexes to gene name i had to recreate a rec assigning dtype (one for cluster).
 
-'''
-names_raw=adata.uns['rank_genes_groups']['names']
+"""
+# # names_raw=adata.uns['rank_genes_groups']['names']
+# names_raw=adata.raw.var_names
 
 
-names_array=[]
+# names_array=[]
 
-for i,v in enumerate(names_raw):  
-    partial=[]
-    for j in range(len(v)):
-        if names_raw[i][j].isdigit():
-            partial.append(adata.raw.var['_index'].iloc[int(names_raw[i][j]),])
-        else:
-            partial.append(names_raw[i][j])
-    names_array.append(partial)    
+# for i,v in enumerate(names_raw):
+#     partial=[]
+#     for j in range(len(v)):
+#         if names_raw[i][j].isdigit():
+#             partial.append(adata.raw.var['_index'].iloc[int(names_raw[i][j]),])
+#         else:
+#             partial.append(names_raw[i][j])
+#     names_array.append(partial)
 
 
+# # 1. i took the group names from anothe dtype in the rank_genes_groups array
+# group_names = adata.uns['rank_genes_groups']['scores'].dtype.names
+# print("group_names:", group_names)
 
-# 1. i took the group names from anothe dtype in the rank_genes_groups array
-group_names = adata.uns['rank_genes_groups']['scores'].dtype.names
-print("group_names:", group_names)
+# # 2. list to numpy array
+# names_np = np.array(names_array)
+# print("names_array shape:", names_np.shape)
+# print("n groups in dtype:", len(group_names))
 
-# 2. list to numpy array
-names_np = np.array(names_array)
-print("names_array shape:", names_np.shape)
-print("n groups in dtype:", len(group_names))
+# # 3. build column based on shape
+# if names_np.ndim == 1:
 
-# 3. build column based on shape
-if names_np.ndim == 1:
-  
-    raise ValueError("names_array ha una sola dimensione, mi aspetto 2D (genes x groups o groups x genes)")
+#     raise ValueError("names_array ha una sola dimensione, mi aspetto 2D (genes x groups o groups x genes)")
 
-if names_np.shape[1] == len(group_names):
-    
-    cols = [names_np[:, i] for i in range(names_np.shape[1])]
-elif names_np.shape[0] == len(group_names):
-   
-    cols = [names_np[i, :] for i in range(names_np.shape[0])]
-else:
-    raise ValueError(
-        f"Shape di names_array {names_np.shape} non compatibile con numero gruppi {len(group_names)}"
-    )
+# if names_np.shape[1] == len(group_names):
 
-# 4. create the rec usin cols as arrays and group names
-names_rec = np.rec.fromarrays(cols, names=group_names)
+#     cols = [names_np[:, i] for i in range(names_np.shape[1])]
+# elif names_np.shape[0] == len(group_names):
 
-print(type(names_rec), names_rec.shape, names_rec.dtype)
+#     cols = [names_np[i, :] for i in range(names_np.shape[0])]
+# else:
+#     raise ValueError(
+#         f"Shape di names_array {names_np.shape} non compatibile con numero gruppi {len(group_names)}"
+#     )
 
-# 5. change to adata
-adata.uns['rank_genes_groups']['names'] = names_rec
+# # 4. create the rec usin cols as arrays and group names
+# names_rec = np.rec.fromarrays(cols, names=group_names)
+
+# print(type(names_rec), names_rec.shape, names_rec.dtype)
+
+# # 5. change to adata
+# adata.uns['rank_genes_groups']['names'] = names_rec
 # ############################################
 
 
-
-#check top markers
+# check top markers
 sc.pl.rank_genes_groups(adata, n_genes=10, sharey=False)
 
 # Top 5 genes for each cluster
-adata.uns['rank_genes_groups']['names']
+adata.uns["rank_genes_groups"]["names"]
 
 # Scores
-adata.uns['rank_genes_groups']['scores']
+adata.uns["rank_genes_groups"]["scores"]
 
 # P-values
-adata.uns['rank_genes_groups']['pvals']
-adata.uns['rank_genes_groups']['logfoldchanges']
+adata.uns["rank_genes_groups"]["pvals"]
+adata.uns["rank_genes_groups"]["logfoldchanges"]
 
 adata.var_names[:10]
 
 
-
 ########## grid UMAP#####################
 for res in [0.02, 0.5, 1.4]:
-    sc.tl.leiden(adata, key_added=f"leiden_res_{res:4.2f}", resolution=res, flavor="igraph")
+    sc.tl.leiden(
+        adata, key_added=f"leiden_res_{res:4.2f}", resolution=res, flavor="igraph"
+    )
 
 
 sc.pl.umap(
@@ -145,10 +147,12 @@ sc.pl.umap(
 )
 
 
-#cluster markers
-sc.tl.rank_genes_groups(adata, groupby='leiden_res_1.40', method='wilcoxon')
+# cluster markers
+sc.tl.rank_genes_groups(adata, groupby="leiden_res_1.40", method="wilcoxon")
 
-sc.pl.rank_genes_groups_dotplot(adata, groupby="leiden_res_1.40", standard_scale="var", n_genes=5,use_raw=False)
+sc.pl.rank_genes_groups_dotplot(
+    adata, groupby="leiden_res_1.40", standard_scale="var", n_genes=5, use_raw=True
+)
 ############ cell type#######################3
 # to look if a gene is present
 
@@ -156,14 +160,14 @@ sc.pl.rank_genes_groups_dotplot(adata, groupby="leiden_res_1.40", standard_scale
 
 [x for x in adata.var_names if "GYPA" in x]
 
-adata.rank_genes_groups['names']
+adata.uns["rank_genes_groups"]["names"]
 
 marker_genes = {
     "CD14+ Mono": ["FCN1", "CD14"],
     "CD16+ Mono": ["TCF7L2", "FCGR3A", "LYN"],
     # Note: DMXL2 should be negative
     "cDC2": ["CST3", "COTL1", "LYZ", "DMXL2", "CLEC10A", "FCER1A"],
-    "Erythroblast": ["MKI67",  "HBB"],
+    "Erythroblast": [ "HBB"],#"MKI67"], gene not found in the set
     # Note HBM and GYPA are negative markers
     "Proerythroblast": ["CDK6", "SYNGR1"],
     "NK": ["GNLY", "NKG7", "CD247", "FCER1G", "TYROBP", "KLRG1", "FCGR3A"],
@@ -184,20 +188,15 @@ marker_genes = {
     "Plasma cells": ["MZB1", "HSP90B1", "FNDC3B", "PRDM1", "IGKC", "JCHAIN"],
     # Note PAX5 is a negative marker
     "Plasmablast": ["XBP1", "PRDM1", "PAX5"],
-    "CD4+ T": ["CD4", "IL7R", "TRBC2"],
+    "CD4+ T": ["CD4", "IL7R"],#, "TRBC2"], # TRBC2 not found in raw
     "CD8+ T": ["CD8A", "CD8B", "GZMK", "GZMA", "CCL5", "GZMB", "GZMH", "GZMA"],
     "T naive": ["LEF1", "CCR7", "TCF7"],
     "pDC": ["GZMB", "IL3RA", "COBLL1", "TCF4"],
 }
 
 
-
 sc.pl.dotplot(
-    adata,
-    marker_genes,
-    groupby="leiden_res_1.40",
-    standard_scale="var",
-    use_raw=False
+    adata, marker_genes, groupby="leiden_res_1.40", standard_scale="var", use_raw=False
 )
 
 
@@ -210,14 +209,15 @@ adata.obs["cell_type_lvl1"] = adata.obs["leiden_res_1"].map(
         "4": "T CD8+",
         "5": "no determined",
         "6": "naive CD20+ B",
-        "7" : "CD14+ Mono",
+        "7": "CD14+ Mono",
         "8": "CD16+ Mono",
-        "9":"plasmacells",
+        "9": "plasmacells",
         "10": "no determined",
-        "11":"no detemined"
-
+        "11": "no detemined",
     }
 )
 # Obtain cluster-specific differentially expressed genes
 sc.tl.rank_genes_groups(adata, groupby="leiden_res_1", method="wilcoxon")
-sc.pl.rank_genes_groups_dotplot(adata, groupby="leiden_res_1", standard_scale="var", n_genes=5, use_raw=False)
+sc.pl.rank_genes_groups_dotplot(
+    adata, groupby="leiden_res_1", standard_scale="var", n_genes=5, use_raw=False
+)
